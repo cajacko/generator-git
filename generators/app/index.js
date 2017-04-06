f7a701d946215c0f7df7bedeb32c60d5e2e81a0d
Charlie Jackson <charlie@charliejackson.com>
2017-04-06 10:15:11 +0100

    Specified when to run git in the yeoman context queue

--- generators/app/index.js
+++ generators/app/index.js
@@ -20,7 +20,7 @@ module.exports = class extends Generator {
     });
   }
 
-  initialiseGit() {
+  configuring() {
     const destinationPath = this.destinationPath();
     const path = `${destinationPath}/.git`;
 
